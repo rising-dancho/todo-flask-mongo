160325c835d4d9bee4e34871531d2546739945df
adfinem <zenlaws02@gmail.com>
2024-12-10 17:09:32 +0800

    fix: imported the Object id from bson.objectid that allows getting the ObjectId from mongodb

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import os
 from dotenv import load_dotenv
+from bson.objectid import ObjectId
 
 # Load environment variables from .env file
 load_dotenv()
